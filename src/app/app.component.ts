e6969bf5e9c42514aec23d338b1226124ea3e781
CypherRat <mailofabhimanyu@gmail.com>
2023-02-16 19:21:28 +0530

    Debouncer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
+
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  inputData!: string;
+  searchSubjectString = new Subject<string>();
+
   constructor(public router: Router) {}
   title: string = 'Books Library App';
   subjectName: string = '';
@@ -19,11 +25,28 @@ export class AppComponent {
     { name: 'Harry Potter' },
     { name: 'Crypto' },
   ];
-  gotosubject(event: any): void {
-    if (event.keyCode !== 13) return;
-    alert(event.keyCode);
-    let searchTerm: any = event.target.value;
-    if (!searchTerm.length) return alert('Enter subject name to search!');
-    this.router.navigate(['/subject/' + searchTerm]);
+
+  ngOnInit(): void {
+    this.searchSubjectString
+      .pipe(debounceTime(500))
+      .subscribe((val: string): any => {
+        if (!val.trim().length) return alert('Enter a subject name to search!');
+        let searchTerm: any = val.trim();
+        this.router.navigate(['/subject/' + searchTerm]);
+      });
   }
+
+  searchStringChanged(_event: string) {
+    this.searchSubjectString.next(_event);
+  }
+
+  ngOnDestroy() {}
+
+  // gotosubject(event: any): void {
+  //   if (event.keyCode !== 13) return;
+  //   alert(event.keyCode);
+  //   let searchTerm: any = event.target.value;
+  //   if (!searchTerm.length) return alert('Enter subject name to search!');
+  //   this.router.navigate(['/subject/' + searchTerm]);
+  // }
 }
